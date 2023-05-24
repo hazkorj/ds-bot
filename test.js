1a9206f0cfe18a8b0044b9f7ff45caaa59f17f42
Василий Долгих <hazkorj@yandex.ru>
2023-05-24 17:25:56 +0500

    none

--- test.js
+++ test.js
@@ -1,4 +1,6 @@
 setInterval(() => {
     console.log('hello world!');
-    console.log(process.env);
+    console.log(process.env.BOT_TOKEN);
+    console.log(process.env.OPENAI_TOKEN);
+    console.log(process.env.NODE_PATH);
 }, 3000);
