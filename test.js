5c633e13c5f87f35b42d3c81c1c226fcb22d148e
Василий Долгих <hazkorj@yandex.ru>
2023-05-24 17:28:19 +0500

    none

--- test.js
+++ test.js
@@ -1,6 +1,6 @@
 setInterval(() => {
     console.log('hello world!');
-    console.log(process.env.BOT_TOKEN);
-    console.log(process.env.OPENAI_TOKEN);
-    console.log(process.env.NODE_PATH);
+    console.log(process.env.BOT_TOKEN || 'bot token not found');
+    console.log(process.env.OPENAI_TOKEN || 'open ai token not found');
+    console.log(process.env.NODE_PATH || 'node path not found');
 }, 3000);
